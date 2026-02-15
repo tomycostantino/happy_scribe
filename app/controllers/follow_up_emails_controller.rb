64fead00f9e7fd1c1d7a40a2e36f609a607ae88e
Tomas Costantino <tomascostantino.au@gmail.com>
2026-02-15 15:43:34 +0100

    refactor: use MeetingScoped concern in FollowUpEmailsController

--- app/controllers/follow_up_emails_controller.rb
+++ app/controllers/follow_up_emails_controller.rb
@@ -1,6 +1,9 @@
 class FollowUpEmailsController < ApplicationController
+  include MeetingScoped
+
+  before_action :set_meeting, only: :show
+
   def show
-    @meeting = Current.user.meetings.find(params[:meeting_id])
     @email = @meeting.follow_up_emails.find(params[:id])
   end
 end
